714e690dc1234f5db16fd362e7bac24e58e5a87e
shivkant dubey <shivkd1908@gmail.com>
2024-03-03 10:33:18 +0530

    accidently wrote es6 in express

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const port = process.env.PORT;
 const session = require("express-session");
 const passport = require("passport");
 const router = require("./Routes/index");
-import { rateLimit } from "express-rate-limit";
+const {rateLimit}=require('express-rate-limit')
 
 const limiter = rateLimit({
   windowMs: 30 * 60 * 1000, 
